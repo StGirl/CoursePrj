29b15a453bde38299a162e8db45ad71af5e8f47e
StGirl <julia_15kova@mail.ru>
2015-02-21 12:25:19 +0500

    Проверка

---
+++ Курсовой/Input.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Курсовой
+{
+    class Input
+    {
+        public static List<string> Time;
+        public Dictionary<string, List<string>> Weekday;
+        public Dictionary<string, Dictionary<string, List<string>>> Arrival;
+        static public string StartKey(string Start)
+        {
+            return Start;
+        }
+        static public string DestinationKey(string Destination)
+        {
+            return Destination;
+        }
+        static public void AddWeek(Dictionary<string, List<string>>Weekdays)
+        {
+            Weekdays.Add("Понедельник", Time);//Запилить раздельную запись для каждого
+            Weekdays.Add("Вторник", Time);
+            Weekdays.Add("Среда", Time);
+            Weekdays.Add("Четверг", Time);
+            Weekdays.Add("Пятница", Time);
+            Weekdays.Add("Суббота", Time);
+            Weekdays.Add("Воскресенье", Time);
+        }
+        static public void AddTime(List<string>Time)
+        {
+            Time.Add(Console.ReadLine());
+        }
+        static public void Sort(List<string> Time)
+        {
+            Time.Sort();//Запилить сортировку для числовой записи времени
+        }
+    }
+}
---
+++ Курсовой/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Курсовой
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, Dictionary<string, List<string>>>> Departures = new Dictionary<string, Dictionary<string, Dictionary<string, List<string>>>>();
+            Dictionary<string, Dictionary<string, List<string>>> Arrivals = new Dictionary<string, Dictionary<string, List<string>>>();
+            Dictionary<string, List<string>> Weekdays = new Dictionary<string, List<string>>();
+            List<string> Time = new List<string>();//Запилить время в численном формате
+            //Time.Add("8:30");
+            //Time.Add("17:10");
+            //Weekdays.Add("Суббота", Time);
+            //Arrivals.Add("Учалы", Weekdays);
+            //Departures.Add("Челябинск", Arrivals);
+            Input.AddWeek(Weekdays);
+            Input.AddTime(Time);
+            //Input.Sort(Time);
+            foreach (KeyValuePair<string, List<string>> kvp in Weekdays)
+            {
+                Console.WriteLine("{0}:",kvp.Key);
+                foreach (string moment in Time)
+                {
+                    Console.WriteLine(moment);
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
